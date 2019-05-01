f2f48150641b2b3fc92fddb0a9cadc27acfcaec7
Carmen Liu <carmen.liujia@gmail.com>
2019-05-01 21:45:16 +1200

    connect with dispatch function

--- functional-programming-projects/2.caloriecounter/src/index.js
+++ functional-programming-projects/2.caloriecounter/src/index.js
@@ -1,5 +1,6 @@
 import hh from 'hyperscript-helpers';
-import h from 'hyperscript'
+import { h, diff, patch } from 'virtual-dom';
+import createElement from 'virtual-dom/create-element';
 
 const { pre, div, h1, button, form, label, input } = hh(h);
 
@@ -24,7 +25,7 @@ function FieldSet(labelText, inputValue, oninput) {
   ])
 }
 
-function buttonSet() {
+function buttonSet(dispatch) {
   return div([
     button({
       className: 'f3 pv2 ph3 bg-blue white bn mr2 dim',
@@ -42,29 +43,38 @@ function buttonSet() {
   ])
 }
 
-function formView(model) {
-  const { description, calories} = model;
-  return form(
+function formView(dispatch, model) {
+  const { description, calories, showForm} = model;
+  if (showForm) {
+    return form(
+      {
+        className: 'v-100 mv2',
+        onsubmit: e=> console.log('submited')
+      },
+      [
+        FieldSet('Meal', description,
+          e => console.log(e.target.value)
+        ),
+        FieldSet('Calories', calories || '',
+          e => console.log(e.target.value)
+        ),
+        buttonSet()
+      ]
+    )
+  }
+  return button(
     {
-      className: 'v-100 mv2',
-      onsubmit: e=> console.log('submited')
+      className: 'f3 pv2 ph3 bg-blue white bn',
+      onclick: () => dispatch(showFormMsg(true)),
     },
-    [
-      FieldSet('Meal', description,
-        e => console.log(e.target.value)
-      ),
-      FieldSet('Calories', calories || '',
-        e => console.log(e.target.value)
-      ),
-      buttonSet()
-    ]
+    'Add Meal',
   )
 }
 
-function view(model) {
+function view(dispatch, model) {
   const div_dom = div({className: 'mv6 center',}, [
     h1({className: 'f2 pv2 bb' }, 'Calorie Counter'),
-    formView(model),
+    formView(dispatch, model),
     pre(JSON.stringify(model, null, 2))
   ]);
   console.log(div_dom)
@@ -72,5 +82,40 @@ function view(model) {
   return div_dom;
 }
 
+const MSGS = {
+  SHOW_FORM: 'SHOW_FORM',
+};
+
+function showFormMsg(showForm) {
+  return {
+    type: MSGS.SHOW_FORM,
+    showForm,
+  };
+}
+function update(msg, model) {
+  switch (msg.type) {
+    case MSGS.SHOW_FORM: {
+      const { showForm } = msg;
+      return { ...model, showForm, description: '', calories: 0 };
+    }
+  }
+  return model
+}
+
+function app(initModel, update, view, node) {
+  let model = initModel;
+  let currentView = view(dispatch, model);
+  let rootNode = createElement(currentView);
+  node.appendChild(rootNode)
+  node.appendChild(view(model))
+  function dispatch(msg) {
+    model = update(msg, model)
+    const updatedView = view(dispatch, model)
+    const patches = diff(currentView, updatedView)
+    rootNode = patch(rootNode, patches)
+    currentView = updatedView
+  }
+}
+
 const node = document.getElementById('app')
-node.appendChild(view(initModel))
+app(initModel, update, view, node)
