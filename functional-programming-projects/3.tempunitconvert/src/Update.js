72a5d0deda9face1783b277979452c3a42ffec13
Carmen Liu <carmen.liujia@gmail.com>
2019-05-12 14:48:00 +1200

    here's the key of this feature, because it contains how we should put function in to action

--- functional-programming-projects/3.tempunitconvert/src/Update.js
+++ functional-programming-projects/3.tempunitconvert/src/Update.js
@@ -34,31 +34,82 @@ export function rightUnitChangedMsg(rightUnit) {
     rightUnit,
   };
 }
+const toInt = R.pipe(parseInt, R.defaultTo(0))
 
 function update (msg, model) {
   switch (msg.type) {
     case MSGS.LEFT_VALUE_INPUT: {
-      // if(msg.leftValue === '')
-      //   return { ...model }
-      // console.log( msg.leftValue )
-      // // return { ...model, msg.leftValue || '' }
-      // const leftValue = toInt(msg.leftValue);
-      return {...model, leftValue: msg.leftValue }
+      const leftValue = toInt(msg.leftValue)
+      return convert({...model, sourceLeft: true, leftValue })
     }
     case MSGS.RIGHT_VALUE_INPUT: {
-      console.log( msg.rightValue )
-      return {...model, rightValue: msg.rightValue}
+      // console.log( msg.rightValue )
+      const rightValue = toInt(msg.rightValue)
+      return convert({...model, sourceLeft: false, rightValue})
     }
     case MSGS.LEFT_UNIT_CHANGED: {
       console.log( msg.leftUnit )
-      return {...model, leftUnit: msg.leftUnit }
+      return convert({...model, sourceLeft: true, leftUnit: msg.leftUnit })
     }
     case MSGS.RIGHT_UNIT_CHANGED: {
       console.log( msg.rightUnit )
-      return {...model, rightUnit: msg.rightUnit }
+      return convert({...model, sourceLeft: false, rightUnit: msg.rightUnit })
     }
   }
   return model;
 }
+
+function convert(model) {
+  const {leftValue, leftUnit, rightValue, rightUnit } = model
+  const [fromUnit, fromTemp, toUnit] =
+    model.leftUnit
+    ? [leftUnit, leftValue, rightUnit]
+    : [rightUnit, rightValue, leftUnit]
+  const otherValue = convertFromToTemp(fromUnit, toUnit, fromTemp)
+
+  return model.sourceLeft
+  ? { ...model, rightValue: otherValue }
+  : { ...model, leftValue: otherValue };
+
+}
+
+function convertFromToTemp(fromUnit, toUnit, temp) {
+  //R.pathOr('N/A', ['a', 'b'], {a: {b: 2}}); //=> 2
+  return R.pathOr(R.identity, [fromUnit, toUnit], UnitConversions)(temp)
+}
+
+function FtoC(temp) {
+  return 5 / 9 * (temp - 32);
+}
+
+function CtoF(temp) {
+  return 9 / 5 * temp + 32;
+}
+
+function KtoC(temp) {
+  return temp - 273.15;
+}
+
+function CtoK(temp) {
+  return temp + 273.15;
+}
+
+const FtoK = R.pipe(FtoC, CtoK);
+const KtoF = R.pipe(KtoC, CtoF);
+
+const UnitConversions = {
+  Celsius: {
+    Fahrenheit: CtoF,
+    Kelvin: CtoK,
+  },
+  Fahrenheit: {
+    Celsius: FtoC,
+    Kelvin: FtoK,
+  },
+  Kelvin: {
+    Celsius: KtoC,
+    Fahrenheit: KtoF,
+  },
+};
 
 export default update;
